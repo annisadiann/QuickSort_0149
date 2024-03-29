9524c3d4e8b17cf5817acd58b6c78f8b59d76d02
annisadiann <annisadian0412@gmail.com>
2024-03-29 10:54:51 +0700

    memasukkan main

--- Algo_29Maret/Algo_29Maret.cpp
+++ Algo_29Maret/Algo_29Maret.cpp
@@ -100,5 +100,14 @@ void display() {
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
-    } 
+    }
+
+    cout << "\n\nNumber of comparasions: " << cmp_count << endl;
+    cout << "Number of data movements: " << mov_count << endl;
+} 
+int main()
+{
+    char ch;
+
+} 
 
